11ce43cd63e39a212348f5006a26be3ba94a2884
Gustavo Batista <batistag395@gmail.com>
2022-08-18 22:21:47 -0300

    Commitando projeto de descontos

---
+++ Desconto_GFT/CalcDesconto.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace Desconto_GFT
+{
+    public class CalcDesconto
+    {
+        public double ValorTotal { get; set; }
+
+        public CalcDesconto(double valorTotal)
+        {
+            ValorTotal = valorTotal;
+        }
+
+        public double ValorComDesconto()
+        {
+            double valorAnalise = ValorTotal - 300;
+            int count = 0;
+            if (ValorTotal >= 300)
+            {
+                while (valorAnalise  >= 0)
+                {
+                    if(count >= 20)
+                    {
+                        break;
+                    }
+                    valorAnalise -= 100.00;
+                    count++;
+                }
+                Console.WriteLine($"Contemplado com desconto de {count}%");
+            }
+            else
+            {
+                Console.WriteLine("O valor da compra nao contempla desconto.");
+            }
+             return ValorTotal -= ValorTotal * (count / 100.00);
+        }
+        public override string ToString()
+        {
+            return 
+                "Valor total = "
+                + ValorComDesconto().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Desconto_GFT/Program.cs
@@ -0,0 +1,10 @@
+﻿using Desconto_GFT;
+
+CalcDesconto desconto;
+
+Console.WriteLine("Digite o valor do produto:");
+double valor = double.Parse(Console.ReadLine());
+
+desconto = new CalcDesconto(valor);
+
+Console.WriteLine(desconto);
